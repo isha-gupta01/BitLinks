5f35a4b26902e2b3c99099e4fcafe0016a32a6d0
Bloomer004 <ishaguptavnsv@gmail.com>
2025-09-01 07:04:27 +0530

    Security Error fixed

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -1,18 +1,19 @@
-import { redirect } from "next/navigation"
+import { useRouter } from "next/navigation"
 import clientPromise from "@/lib/mongodb"
 
 
 export default async function Page({params}){
+    const router = useRouter();
     const shorturl = (await params).shorturl
     const client = await clientPromise;
     const db = client.db("BitLinks") 
     const collection = db.collection("url")
     const doc = await collection.findOne({shorturl:shorturl})
     if(doc){
-        redirect(doc.url)
+        router.push(doc.url)
     }
     else{
-        redirect(`${process.env.NEXT_PUBLIC_HOST}`)
+        router.push(`${process.env.NEXT_PUBLIC_HOST}`)
     }
     return <div>My Webpage: {shorturl}</div>
 
